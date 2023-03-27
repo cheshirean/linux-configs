97b8fd362e315435c50cd7ab52aca1f684b05347
cheshirean <cheshirean@protonmail.com>
2023-03-27 11:12:44 +0400

    [nvim]: Change layout settings for Telescope
    Update settings for Telescope. Change settings for layout and add stubs
    for extencions and pendings.

--- nvim/lua/cheshirean/plugins/telescope.lua
+++ nvim/lua/cheshirean/plugins/telescope.lua
@@ -1,3 +1,22 @@
+require('telescope').setup{
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-h>"] = "which_key"
+            }
+        },
+        layout_strategy = 'vertical',
+        layout_config = {
+            height = 0.95,
+            width = 0.8,
+        },
+    },
+    pickers = {
+    },
+    extensions = {
+    }
+}
+
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
