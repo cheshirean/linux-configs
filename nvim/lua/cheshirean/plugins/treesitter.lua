79e907de1fdcf4cd22af53c01fe9a48c6378c798
Cheshirean <cheshirean@protonmail.com>
2023-05-02 21:49:47 +0400

    [nvim][treesitter]: Add toml to default config
    Added toml (cargo comfig file) to the default treesitter config

--- nvim/lua/cheshirean/plugins/treesitter.lua
+++ nvim/lua/cheshirean/plugins/treesitter.lua
@@ -12,6 +12,7 @@ require'nvim-treesitter.configs'.setup {
       "make",
       "python",
       "rust",
+      "toml",
   },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
