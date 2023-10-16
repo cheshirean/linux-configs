d81c7bbef52d375acfd84a0610521a46c02ae389
Cheshirean <cheshirean@protonmail.com>
2023-10-17 00:18:04 +0400

    [nvim]: Add vim-be-good plugin
    The vim-be-good plugin is good trainig to vim keys.

--- nvim/lua/cheshirean/plugins-setup.lua
+++ nvim/lua/cheshirean/plugins-setup.lua
@@ -68,6 +68,8 @@ return require('packer').startup(function(use)
         }
     }
 
+    use {'ThePrimeagen/vim-be-good'}
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
